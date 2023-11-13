b49d3eaf2f9a44c71ee5cfcf478c5e223ce10631
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2023-11-13 20:41:57 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple game made on java and based on my previous game, T-Game.
---
+++ launcher.java
@@ -0,0 +1,7 @@
+import TGame2.*;
+public class launcher {
+    public static void main(String[] args) {
+        System.out.println("Запуск игры...");
+        Main.engine();
+    }
+}
